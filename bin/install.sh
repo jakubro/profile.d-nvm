52ea122e602695613945ed81592d5de267692a1c
Jakub Roman <jakub@jakubroman.sk>
2023-11-27 13:16:25 +0100

    error handling

--- bin/install.sh
+++ bin/install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-set -eo pipefail
 
 if [ ! -d ~/.nvm ]; then
 
   log_info "Installing nvm..."
 
-  curl -s https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+  curl -s https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash || exit 1
+
+  log_info "You can safely ignore any instructions printed above"
+
+else
+
+  log_info "Updating nvm..."
+
+  cd ~/.nvm || exit 1
+
+  git fetch --tags origin || exit 1
+
+  nvm_hash=$(git rev-list --tags --max-count=1) || exit 1
+  nvm_version=$(git describe --abbrev=0 --tags --match "v[0-9]*" "$nvm_hash") || exit 1
+
+  git checkout "$nvm_version" || exit 1
 
 fi
-
-log_info "Updating nvm..."
-
-cd ~/.nvm
-
-git fetch --tags origin
-
-nvm_hash=$(git rev-list --tags --max-count=1)
-nvm_version=$(git describe --abbrev=0 --tags --match "v[0-9]*" "$nvm_hash")
-
-git checkout "$nvm_version"
-
-log_info "You can safely ignore any instructions printed above"
